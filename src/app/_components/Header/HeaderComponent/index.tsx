51bc98874747d41d7bbc46e42d7b98bcb16f9a68
Joe Kariuki <jknguru6@gmail.com>
2023-12-21 11:38:09 +0300

    Update header component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,16 +1,37 @@
+'use client'
+
 import React from 'react'
 
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
   return (
-    <nav>
-      <Gutter>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" />
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            className={classes.logo}
+            width={170}
+            height={50}
+          />
         </Link>
+        <HeaderNav header={header} />
       </Gutter>
     </nav>
   )
